f2397d44bc6fda65ee8d7baf6a07e33cf2d49b93
Nick Irvine <nirvine@hotheadgames.com>
2014-08-17 16:11:42 -0700

    Add ip arg

--- netgeardisc.py
+++ netgeardisc.py
@@ -1,8 +1,15 @@
 import socket
 import time
+import sys
+
+if len(sys.argv) < 2 or sys.argv[1] in ('-h', '--help'):
+    print('netgeardisc <my IP>')
+    sys.exit(1)
+else:
+    me = sys.argv[1]
+
 
 p = b'000200000000000000000000000100000c07d2f20000000000000000000000000000000000000000'
-me = '192.168.9.201'
 
 sout = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sout.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
